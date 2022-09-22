8fe0771fe197d9bbdbec0e705cbfd66a282f08f1
Azamat <isabirovazamati@gmail.com>
2022-09-22 18:05:55 +0500

    Add hello world

---
+++ Python001_Hello_World/main.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+value = None
+print(type(value))
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 12334
+print(value)
+print(type(value))
+s='hello world'
+print(s) # вывод строки
